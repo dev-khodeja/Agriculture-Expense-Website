458801beef94441e9d34d29782bd895d4c545b0c
khodeja Aktar <khodejaaktar713@gmail.com>
2026-05-22 18:02:36 +0600

    url

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 const app = express();
 
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(cors({ origin: 'https://agriculture-expense-website.vercel.app', credentials: true }));
 app.use(express.json());
 
 // Routes
